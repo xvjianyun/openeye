d095aaebaf3fed42a2c2e206551f9c3ee41affcf
许建云 <1653275855@qq.com>
2020-11-28 08:10:23 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+const autoprefixer = require("autoprefixer");
+const pxtoviewport = require("postcss-px-to-viewport");
+
+module.exports = {
+  outputDir: "dist",
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          autoprefixer(),
+          pxtoviewport({
+            viewportWidth: 375
+          })
+        ]
+      }
+    }
+  },
+  devServer: {
+    port:8080,
+    host:'localhost',
+    open:true,
+    proxy: {
+      '^/api': {
+        target: 'http://baobab.kaiyanapp.com',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        }
+      }
+    }
+  }
+}
